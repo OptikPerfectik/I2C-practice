cdd0fa9ed10c0582eaeb4066c309f691482bac4e
user <user@user-Aspire-E5-721>
2020-05-30 18:52:55 -0400

    Initial commit of project

---
+++ HelloWorldReview/src/HelloWorldReview.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : HelloWorldReview.cpp
+// Author      : TomB
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!sdf!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
